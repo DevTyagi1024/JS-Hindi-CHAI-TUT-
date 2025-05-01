014bb805de33578e1391e7e95de076e680461583
DevTyagi1024 <157288481+DevTyagi1024@users.noreply.github.com>
2025-05-01 16:11:39 +0000

    Datatypes

---
+++ Tut-Folder/02_datatypes.js
@@ -0,0 +1,19 @@
+"use strict"  // treat all the js code as newer version
+
+// alert(3+3)  //This will gave us error because we are using the noda js not browser
+
+console.log(3 + 3)
+//This space is for better code readability
+console.log("Dev Tyagi")
+
+let name = "Dev"  // Data type = string ""
+let age = 23    // Data type = number
+let isloggedIn = true  // Datatype = boolean
+let state  //  Datatype = undefined
+let temp = null  //  Datatype = null(not empty or zero)
+  //Object
+  //symbols
+  //Null is an object
+
+  console.log(typeof null)
+  console.log(typeof undefined)
