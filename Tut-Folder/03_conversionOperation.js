8a6e3f39fc88dc1f91a6c64f8170daf7144bbdbc
DevTyagi1024 <157288481+DevTyagi1024@users.noreply.github.com>
2025-05-04 13:24:08 +0000

    Conversion operations

---
+++ Tut-Folder/03_conversionOperation.js
@@ -0,0 +1,27 @@
+// let score = "33abc";
+
+// console.log(typeof score);
+
+// console.log(typeof (score));
+
+// let valueInNumber = Number(score);
+
+// console.log(typeof valueInNumber)
+
+// console.log(valueInNumber)
+
+let isLoggedIn = 1
+
+let changedLoggedIn = Boolean(isLoggedIn)
+
+// console.log(changedLoggedIn)
+
+// console.log(typeof changedLoggedIn)
+
+let someNumber= 44
+
+let stringNumber = String(someNumber)
+
+console.log(stringNumber)
+
+console.log(typeof stringNumber)
