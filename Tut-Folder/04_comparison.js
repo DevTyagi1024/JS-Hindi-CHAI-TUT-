d62d7d392e8a4293968c58cb5e48c30f4eb99e3f
DevTyagi1024 <157288481+DevTyagi1024@users.noreply.github.com>
2025-05-10 11:46:18 +0000

    Comparison

---
+++ Tut-Folder/04_comparison.js
@@ -0,0 +1,16 @@
+// console.log("2" > 1);
+
+// console.log("02" > 1)
+
+console.log(null > 0)     // This will give the output false
+console.log(null == 0)    // This will give the output false
+console.log(null >= 0)    // This will give the output true the reason is that the operator == and < > <= >= works differently comparison converts null into a 0 thats why for 3 one it is giving true   
+
+
+// console.log(undefined == 0)
+// console.log(undefined > 0)
+// console.log(undefined < 0)
+
+// "===" the operator that check the data types in the comparison as well
+
+console.log("02" === 2)
