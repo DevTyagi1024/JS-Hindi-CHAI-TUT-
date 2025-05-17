a45f20a16d533e212938f709e5173cbe2be195a8
DevTyagi1024 <157288481+DevTyagi1024@users.noreply.github.com>
2025-05-17 12:21:16 +0000

    Math and num

---
+++ Tut-Folder/07_nums_maths.js
@@ -0,0 +1,31 @@
+// const balance = 400
+// console.log(balance)
+
+// const score = new Number(100)
+// console.log(score)
+
+// console.log(balance.toString())
+// console.log(balance.toFixed(3))
+
+// const otherNumber = 12.345
+// console.log(otherNumber.toPrecision(3))
+
+// const hundreds= 10000
+// console.log(hundreds.toLocaleString('en-IN'))
+
+
+// Maths
+
+// console.log(Math.abs(-4))
+// console.log(Math.round(4.6))
+// console.log(Math.ceil(4.2))
+// console.log(Math.floor(4.2))
+// console.log(Math.min(3,5,2,5))
+
+console.log(Math.random())
+console.log((Math.random()*10) +1)
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max-min+1)) + min)
