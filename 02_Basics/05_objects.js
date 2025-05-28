11e20eb06a73f7888418743e11cc01b86f7e0c76
DevTyagi1024 <157288481+DevTyagi1024@users.noreply.github.com>
2025-05-28 01:55:10 +0000

    Objects

---
+++ 02_Basics/05_objects.js
@@ -0,0 +1,15 @@
+const course = {
+    coursename :"js",
+    courseprice:"2000",
+    courseinstructtor:"Dev"
+}
+
+const {courseinstructtor} = course
+
+console.log(courseinstructtor);
+
+
+{
+    {}
+    {}
+}
