4ad7fc9060e87c5c3ed92fe195978462f63bc11b
DevTyagi1024 <157288481+DevTyagi1024@users.noreply.github.com>
2025-05-29 02:01:38 +0000

    Functions

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,26 @@
+function myName() {
+    console.log("D");
+    console.log("E");
+    console.log("V");
+}
+
+myName()
+
+function addition(num1, num2) {
+    let result = num1 + num2;
+    return result;
+}
+
+const result = addition(10, 3)
+console.log("Result :", result)
+
+
+function loginUserMessage(username) {
+    if (username === undefined) {
+        console.log("Please enter a username");
+        return
+    }
+    return `${username} Just logged In`
+}
+
+console.log(loginUserMessage());
