9549a515232462ce7852376e7507a1fcb565db85
DevTyagi1024 <157288481+DevTyagi1024@users.noreply.github.com>
2025-05-29 03:20:36 +0000

    Object and array handling in functions

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -24,3 +24,42 @@ function loginUserMessage(username) {
 }
 
 console.log(loginUserMessage("Dev"));
+
+
+function calculateCartPrice(...num1) {
+    return num1
+}
+
+console.log(calculateCartPrice(200, 300, 22, 3, 24,));
+
+
+
+
+
+const User ={
+    name:"Dev",
+    salary: 400000
+}
+
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.name} and the salary is ${anyobject.salary}`);
+}
+
+console.log(handleObject({
+    name:"Aman",
+    salary:2000000
+}));
+
+
+
+const myArray = [200,300,400]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+console.log(returnSecondValue(myArray));
+
+
+
+
