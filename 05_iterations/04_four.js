4d3c461d28c773195c6d3520ed1abfd40d98867c
DevTyagi1024 <157288481+DevTyagi1024@users.noreply.github.com>
2025-06-18 06:06:24 +0000

    For in loop

---
+++ 05_iterations/04_four.js
@@ -0,0 +1,16 @@
+const myObject = {
+    js: "Javascript",
+    cp: "C++"
+}
+for (const key in myObject) {
+    console.log(`${key} for the ${myObject[key]}`);
+
+}
+
+
+
+const programming = ["C++","Java","Python"]
+for (const key in programming) {
+   console.log(programming[key]);
+   
+}
