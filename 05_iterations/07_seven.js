1dcaa47be60083107fb23ac16c91ea6dd6d366fc
DevTyagi1024 <157288481+DevTyagi1024@users.noreply.github.com>
2025-06-25 01:56:27 +0000

    map plus filter

--- 05_iterations/07_seven.js
+++ 05_iterations/07_seven.js
@@ -1,6 +1,13 @@
 const mynumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-const newNums = mynumbers.map((num) =>
-    num + 10)
+// const newNums = mynumbers.map((num) =>
+//     num + 10)
 
-console.log(newNums);
+const newNums = mynumbers
+    .map((num) => num * 10)
+    .map((num) => num + 2)
+    .filter((num)=> num>=80)
+
+console.log(newNums)
+
+
