7811fd369fd01041dad636326e6016ca6ba896da
DevTyagi1024 <157288481+DevTyagi1024@users.noreply.github.com>
2025-10-25 14:08:31 +0000

    Promises

---
+++ 08_advance-one/promises.js
@@ -0,0 +1,91 @@
+new Promise(function (resolve, reject) {
+
+    // Do an async task
+
+    setTimeout(function () {
+        console.log(alert());
+        resolve();
+    }, 1000)
+
+}).then(function () {
+    console.log("Promise consumed");
+})
+
+
+// Second promise to understand the basics
+
+let myPromise = new Promise(function (resolve, reject) {
+    let success = true;
+
+    if (success) {
+        resolve();
+    }
+
+    else {
+        reject();
+    }
+})
+
+myPromise
+    .then(function (message) {
+        console.log("Database Connected");
+    })
+    .catch(function (error) {
+        console.log("Database is not connected");
+    })
+    .finally(function (error) {
+        console.log("The promise is either resolved or rejected");
+    });
+
+
+// Third promise for some advanced information
+
+const promiseThree = new Promise(function (resolve, reject) {
+    setTimeout(function () {
+        resolve({
+            username: "Devtyagi638@gmail.com",
+            Name: "Dev Tyagi",
+        })
+    }, 1000)
+})
+
+promiseThree
+    .then(function (user) {
+        console.log(user);
+
+    });
+
+
+// Fourth promise with chaining of then()
+
+const promiseFour = new Promise(function (resolve, reject) {
+    setTimeout(function () {
+        let error = true;
+
+        if (error == true) {
+            resolve({
+                username: "aman123@gmail.com",
+                id: "123",
+            })
+        }
+        else {
+            reject("Error")
+        }
+    }, 1000)
+})
+
+promiseFour
+    .then(function (user) {
+        console.log(user);
+        return user.username
+    })
+    .then(function (username) {
+        console.log(username);
+
+    })
+    .catch(function (message) {
+        console.log(message);
+    })
+    .finally(function () {
+        console.log("The promise is either resolved or rejected");
+    });
