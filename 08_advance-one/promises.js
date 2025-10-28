60f7eb6e7a10082c5a48b98144ca6d7186ee90fe
DevTyagi1024 <157288481+DevTyagi1024@users.noreply.github.com>
2025-10-28 03:04:25 +0000

    Promise with fetch

--- 08_advance-one/promises.js
+++ 08_advance-one/promises.js
@@ -89,3 +89,51 @@ promiseFour
     .finally(function () {
         console.log("The promise is either resolved or rejected");
     });
+
+
+// Promise five
+
+
+const promiseFive = new Promise(function (resolve, reject) {
+    setTimeout(function () {
+
+        let error = true;
+
+        if (error == true) {
+            resolve({
+                username: "tyagi123@gmail.com",
+                id: "934",
+            })
+        }
+        else {
+            reject("New Error")
+        }
+    }, 1000)
+});
+
+async function consumepromise() {
+    try {
+        const response = await promiseFive;
+        console.log(response);
+    }
+    catch (error) {
+        console.log(error)
+    }
+}
+
+consumepromise();
+
+
+async function getallusers() {
+    try {
+        const response = await fetch('https://jsonplaceholder.typicode.com/users');
+        const data = await response.json();
+        console.log(data)
+    }
+
+    catch {
+        console.log("Fetch error")
+    }
+}
+
+getallusers();
